ceafc2d86669760bc565448b7eaf9ebbac4a5b2f
Andres <acastastanedaa@worldbank.org>
2022-12-22 17:46:37 -0500

    init documentation

--- R/st_write.R
+++ R/st_write.R
@@ -1,21 +1,22 @@
 #' Write R object with corresponding stamp (hash digest)
 #'
 #' @param x R object to write to disk as per limitations of `file` format.
-#' @param file File or connection to write to
-#' @param ext
-#' @param st_file
-#' @param attr
-#' @param ...
+#' @param file character: File or connection to write to
+#' @param ext  character: format or extension of file. Default is
+#'   `fs::path_ext(file)`
+#' @param st_file character: Directory to store stamp files. By default it is a
+#'   subdirectory at the same level of `file`.
+#' @param attr list of attributes to store
+#' @param ... not is used right now
 #'
-#' @section Details:
-#' Object `x` is stored in `file` but its hash (i.e., stamp) is
-#' stored in subdirectory `st_file`.
-#'
-#'
-#'
+#' @section Details: Object `x` is stored in `file` but its hash (i.e., stamp)
+#'   is stored in subdirectory `st_file`.
 #'
 #'
-#' @return
+#'
+#'
+#'
+#' @return TRUE is object was saved successfully. FALSE otherwise.
 #' @export
 #'
 #' @examples
@@ -23,7 +24,7 @@ st_write <- function(x,
                      file,
                      ext = fs::path_ext(file),
                      st_file = "_stamp",
-                     attr = NULL,
+                     attr = list(),
                      ...) {
 
 #   ____________________________________________________
